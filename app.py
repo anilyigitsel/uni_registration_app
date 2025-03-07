a455344d1c839008627c66a783cdcfe0b8488946
starklesss <anilsel24@gmail.com>
2025-03-07 12:22:07 +0300

    Defining the functions

---
+++ app.py
@@ -0,0 +1,18 @@
+def login():
+    pass
+
+def academic_records():
+    pass
+
+def course_list_preparation():
+    pass
+
+def gpa_calculator():
+    pass
+
+def student_rep_elections():
+    pass
+
+print("Welcome to University Registration")
+
+
