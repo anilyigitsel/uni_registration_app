9a8aa7a5d604629aa21cfdf30211d68302ec76cf
starklesss <anilsel24@gmail.com>
2025-03-07 13:08:29 +0300

    added the logout choice

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 def login():
     int(input("Student Number: "))
     input("Password: ")
-    print("Succesfully login!")
+    print("Succesfully logged in!")
 
 def academic_records():
     pass
@@ -15,19 +15,40 @@ def gpa_calculator():
 def student_rep_elections():
     pass
 
+login()
+
 isRunning = True
 
 while isRunning:
     print("Welcome to University Registration!")
-    login()
+    print("------------")
     print("Registration")
     print("------------")
-    print("Enter your choice (1-5): ")
     print("1. Academic Records")
     print("2. Course List Preparation")
     print("3. GPA Calculator")
     print("4. Student Representative Elections")
-    print("5. Exit")
+    print("5. Logout")
+    print("6. Exit")
+    choice = input("Enter your choice (1-6): ")
+
+    if choice =="1":
+        academic_records()
+    elif choice == "2":
+        course_list_preparation()
+    elif choice == "3":
+        gpa_calculator()
+    elif choice == "4":
+        student_rep_elections()
+    elif choice == "5":
+        login()
+    elif choice == "6":
+        isRunning = False
+    else:
+        print("Select a valid choice!")
+
+
+
 
 
 
